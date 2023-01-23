d83e6e13508a7e287224f17e2a8fe4a62d9e37e8
Daniel abedi <120008119+danielabedi@users.noreply.github.com>
2023-01-23 14:30:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let Contacts=[];
+let bouton1 = document.querySelector('.bout1')
+let bouton2 = document.querySelector('.bout2')
 const form = document.querySelector('form');
 const idInput = document.querySelector('#Id');
 const firstNameInput = document.querySelector('#Prénom');
@@ -16,7 +18,93 @@ let avatarElement = document.createElement('img');
 let infoElement = document.createElement('div');
 let confirm;
 
+//=======First Name Validation=========
+function validPrenom(){
+  let prenom = form.elements.Prénom.value
+  if (prenom.length < 3) {
+      if (prenom.length === 0) {
+          document.getElementById("Prénom").style.borderColor = "";
+          document.getElementById("prenomErr").style.color=""
+          document.getElementById("prenomErr").innerHTML = "";
+        }else{
+      document.getElementById("Prénom").style.borderColor = "red";
+      document.getElementById("prenomErr").style.color="red"
+      document.getElementById("prenomErr").innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>renseigner un nom avec plus de 3 caractères</p>`;
+  }
+    }else if (prenom.length > 50) {
+      document.getElementById("Prénom").style.borderColor = "red";
+      document.getElementById("prenomErr").style.color="red"
+      document.getElementById("prenomErr").innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>renseigner un nom avec moin de 50 caractères</p>`;
+    }else{
+      document.getElementById("Prénom").style.borderColor = "";
+          document.getElementById("prenomErr").style.color=""
+          document.getElementById("prenomErr").innerHTML = "";
+    }
+}
 
+//=============Name Validation==============
+function validNom(){
+  let nom = form.elements.Nom.value
+  if (nom.length < 3) {
+      if (nom.length === 0) {
+          document.getElementById("Nom").style.borderColor = "";
+          document.getElementById("nomErr").style.color=""
+          document.getElementById("nomErr").innerHTML = "";
+        }else{
+      document.getElementById("Nom").style.borderColor = "red";
+      document.getElementById("nomErr").style.color="red"
+      document.getElementById("nomErr").innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>renseigner un nom avec plus de 3 caractères</p>`;
+  }
+    }else if (nom.length > 50) {
+      document.getElementById("Nom").style.borderColor = "red";
+      document.getElementById("nomErr").style.color="red"
+      document.getElementById("nomErr").innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>renseigner un nom avec moin de 50 caractères</p>`;
+    }else{
+      document.getElementById("Nom").style.borderColor = "";
+          document.getElementById("nomErr").style.color=""
+          document.getElementById("nomErr").innerHTML = "";
+    }
+}
+
+//==========Validation Number=========
+function validPhone(){
+  let Telephone = form.elements.Telephone.value;
+  let phoneRegex = /^\d+$/;
+  let validPrefixes = ["084", "085", "080", "089", "081", "082", "083", "099", "097", "097", "090"];
+  if(!phoneRegex.test (Telephone)){
+    document.getElementById('Telephone').style.borderColor ='red';
+    document.getElementById('phoneErr').style.color='red';
+    document.getElementById('phoneErr').innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>Renseigner un numéro de téléphone valide</p>`;
+  }else if (Telephone.length !==10){
+    document.getElementById('Telephne').style.borderColor = 'red';
+    document.getElementById('phoneErr').style.color = 'red';
+    document.getElementById('phoneErr').innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>Renseigner un numéro de téléphone avec au moins 10 chiffres</p>`;
+  }else if (!validPrefixes.includes(Telephone.substring(0, 3))) {
+    document.getElementById('Telephone').style.borderColor = 'red';
+    document.getElementById('phoneErr').style.color = 'red';
+    document.getElementById('phoneErr').innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>Renseigner un numéro de téléphone au format valide</p>`;
+  }else{
+    document.getElementById('Telephone').style.borderColor ='';
+    document.getElementById('phoneErr').style.color ='';
+    document.getElementById('phoneErr').innerHTML = '';
+  }
+}
+// =======validation email=========
+function validEmail() {
+  let email = form.elements.email.value
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  if (!emailRegex.test(email)) {
+      document.getElementById("email").style.borderColor = "red";
+      document.getElementById("errorEmail").style.color="red"
+      document.getElementById("errorEmail").innerHTML = `<p><span class="warning__Icon"><i class="fa-solid fa-circle-exclamation"></i></span>Veuillez saisir une adresse email valide</p>`;
+      return false;
+    } else {
+      document.getElementById("email").style.borderColor = "";
+      document.getElementById("errorEmail").style.color="";
+      document.getElementById("errorEmail").innerHTML = "";
+      return true;
+    }
+}
 form.addEventListener('submit', event => {
   event.preventDefault();
   console.log( 'debut'+ idInput.value);
@@ -49,8 +137,7 @@ form.addEventListener('submit', event => {
         editContactToList(contact);
       }
       
-  }else
-  {
+  }else {
     const id = Contacts.length + 1;
     const firstName = firstNameInput.value;
     const lastName = lastNameInput.value;
@@ -82,18 +169,13 @@ form.addEventListener('submit', event => {
         editContactToList(contact);
       }
   }
- 
-  
-function editContactToList(contact)
-{
+ function editContactToList(contact){
     if (contact.avatar == './avatar.png') {
         avatarElement.src = contact.avatar;
-        //contactList.removeChild(contactElement);
       } else {
         avatarElement.src =  URL.createObjectURL(contact.avatar);
-        //contactList.removeChild(contactElement);
       }
-      //contactElement.appendChild(avatarElement);
+
       infoElement.innerHTML = `
       <h4>${contact.firstName} ${contact.lastName}</h4>
       <p>Téléphone: ${contact.phone}</p>
@@ -101,59 +183,35 @@ function editContactToList(contact)
       <p>Email: <a href="mailto:${contact.email}?subject=Bonjour%20${contact.firstName}%20${contact.lastName}&body=Bonjour%20${contact.firstName},%0A%0A%0A%0A%0A%0A%0A%0AMes%20meilleures%20salutations%0A%0A${contact.lastName}">${contact.email}</a></p>
       <p>Bio: ${contact.bio}</p>
       `;
-  
-   // contactElement.appendChild(infoElement);
-
-
- 
 }
- 
 
- 
-
-  // Création de l'objet contact avec les données du formulaire
- 
-  
-  // Ajout du contact à la liste des contacts
- 
-
-
-  // Réinitialisation du formulaire
+  // ============Réinitialisation du formulaire=======
   form.reset();
   resetButton.addEventListener('click', () => {
     form.reset();
   });
 });
 
-
-
-
-
-
-
 function addContactToList(contact) {
-  // Création de l'élément HTML pour le nouveau contact
+  // ============Création de l'élément HTML pour le nouveau contact=============
   contactElement = document.createElement('div');
   
   contactElement.classList.add('contact');
 
-  // Ajout de l'avatar
+  //==============Ajout de l'avatar==============
   avatarElement = document.createElement('img');
-//   avatarElement.style.height='200px';
-//   avatarElement.style.width='200px';
   if (contact.avatar == './avatar.png') {
     avatarElement.src = contact.avatar;
-    //contactList.removeChild(contactElement);
+    
   } else {
     avatarElement.src =  URL.createObjectURL(contact.avatar);
-    //contactList.removeChild(contactElement);
   }
   
   avatarElement.classList.add('avatar');
   contactElement.appendChild(avatarElement);
   infoElement = document.createElement('div');
 
-  // Ajout des informations du contact
+  // ========Ajout des informations du contact==============
   
   infoElement.classList.add('info');
   infoElement.innerHTML = `
@@ -164,16 +222,26 @@ function addContactToList(contact) {
     <p>Bio: ${contact.bio}</p>
     `;
   contactElement.appendChild(infoElement);
+  
  
+  // =============Ajout du bouton de modification============
 
- 
-  // Ajout du bouton de modification
   const modifyButton = document.createElement('button');
   modifyButton.innerHTML = '<a class="icon iconnoir " href="#"> <i class="fa-solid fa-user-pen"></i></a>';
   modifyButton.classList.add('modify');
   modifyButton.addEventListener('click', () => {
- 
-    // Populate the form with the current contact's information
+
+    bouton1.innerHTML = 'Modifier'
+    bouton2.innerHTML = 'Annuler'
+    // ===========Récuperer le bouton créer et renit ==============
+     bouton1.addEventListener('click', function(){
+      
+        bouton1.innerHTML = 'Créer'
+        bouton2.innerHTML = 'Rénit'
+     })
+     
+    //============== Populate the form with the current contact's information============
+
     if (contact.avatar == './avatar.png') {
         idInput.value=contact.id;
         firstNameInput.value = contact.firstName;
@@ -182,8 +250,7 @@ function addContactToList(contact) {
         groupSelect.value = contact.group;
         emailInput.value = contact.email;
         bioInput.value = contact.bio;
-        avatarInput.value = contact.avatar;
-        
+        avatarInput.value = contact.avatar; 
       } else {
         idInput.value=contact.id;
         firstNameInput.value = contact.firstName;
@@ -193,149 +260,56 @@ function addContactToList(contact) {
         emailInput.value = contact.email;
         bioInput.value = contact.bio;
         avatarInput.value = URL.createObjectURL(contact.avatar);
-        //contactList.removeChild(contactElement);
       }
-
-    
   });
   contactElement.appendChild(modifyButton);
-  
-  // Ajout du bouton de suppression
-  
-      
+
+  // =========Ajout du bouton de suppression=============
+
       const deleteButton = document.createElement('button');
-      deleteButton.innerHTML = ' <a class="icon iconred " href="#"><i class="fa-regular fa-trash-can"></i></a>';
+      const confirmDelete = document.getElementById('button')
+
+      deleteButton.innerHTML = ' <a class="icon iconred " id = "buttonDelete" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-trash-can" ></i></a>';
       deleteButton.classList.add('delete');
-      deleteButton.addEventListener('click', () => {
-      confirm('Voulez-vous supprimer ce contact?');
-   
-    contactList.removeChild(contactElement);
+      deleteButton.addEventListener('click',() => {
+        confirmDelete.innerHTML = `<a class="icon iconred btn btn-danger" href="#">Oui</a>`;
+        confirmDelete.addEventListener('click', function(){
+          contactList.removeChild(contactElement);
+        });
 
   });
-  contactElement.appendChild(deleteButton);
- 
-  
 
-  // Ajout du contact à la liste des contacts
+  contactElement.appendChild(deleteButton);
+  //=========== Ajout du contact à la liste des contacts=========
   contactList.appendChild(contactElement);
 }
-avatarInput.addEventListener('change', event => {
+
+
+avatarInput.addEventListener('dragover', event => {
     const avatar = event.target.files[0];
   
-    // Vérification de l'extension de l'avatar
+    // ============Vérification de l'extension de l'avatar==========
     if (!avatar.type.match('image.*')) {
       alert('Seules les images sont autorisées !');
       return;
     }
   
-    // Mise à jour de l'avatar prévisualisé
+    // ===========Mise à jour de l'avatar prévisualisé===========
     const avatarPreview = document.querySelector('.avatar-preview');
     avatarPreview.src = URL.createObjectURL(avatar);
     avatarPreview.classList.remove('hidden');
   });
 
-  // Masquage de la prévisualisation de l'avatar au chargement de la page
+  // ===========Masquage de la prévisualisation de l'avatar au chargement de la page===========
 document.addEventListener('DOMContentLoaded', () => {
     const avatarPreview = document.querySelector('.avatar-preview');
     avatarPreview.src = '';
     avatarPreview.classList.add('hidden');
   });
 
-
+  
+ 
 
  
   
-function afficherContacte()
-{
-let listeContacts= document.getElementById('col');
-let i=0;
-let body= document.body;
-    //  console.log(pp);
-        for (i ;i < contacts.length;i++) {
-            let div= document.createElement("div");
-            let aSupprimer =document.createElement("a");
-            let aModifier =document.createElement("a");
-            let iSupprimer =document.createElement("i");
-            let iModifier =document.createElement("i");
-            div.className = 'contact';
-            listeContacts.append(div);
-            div.append(aSupprimer);
-            div.append(aModifier);
-            aSupprimer.append(iSupprimer);
-            aModifier.append(iModifier);
-            div.className = 'd-flex flex-row mb-3 row-gap-4';
-            iSupprimer.className = 'fa-solid fa-trash';
-            iModifier.className = 'fa-solid fa-pen-to-square';
-            lienSetAttribut=document.getElementsByClassName('a');
-            divContact=document.getElementsByClassName('contact');
-            aModifier.setAttribute('href','#');
-            aSupprimer.setAttribute('href','#');
-            iSupprimer.textContent= contacts[i]['id'];
-            iModifier.textContent= contacts[i]['id'];
-            iSupprimer.addEventListener('click', ()=>{
-                contacts.forEach(contact => {
-                    if(contact['id'] == iSupprimer.textContent)
-                   {
-                    contacts.splice(contact['id'],1);
-                    div.remove(aSupprimer);
-                    
-                   }
-                });
-                
-
-            } ); 
-            iModifier.addEventListener('click', ()=>{
-              
-             
-               contacts.forEach(contact => {
-                if(contact['id'] == iModifier.textContent)
-               {
-                // Affichage des informations dans le formulaire du contact selectionner
-                document.getElementById('Id').value = contact['id'] ;
-                document.getElementById('Prenom').value = contact['prenom'] ;
-                document.getElementById('Nom').value = contact['nom'] ;
-                document.getElementById('Telephone').value = contact['telephone'] ;
-                document.getElementById('Groupe').value = contact['groupe'] ;
-                document.getElementById('Bio').value = contact['bio'] ;
-                //document.getElementById('Avatar').value = contact['photo'] ;
-                document.getElementById('Email').value = contact['email'] ;
-                //console.log(contacts)
-                // Recuperation de valeurs des input modifiees
-
-                const form = document.querySelector("form");
-                form.addEventListener('submit', () =>{
-                let id = document.getElementById('Id').value;
-                let prenom = document.getElementById('Prenom').value;
-                let nom=document.getElementById('Nom').value ;
-                let telephone=document.getElementById('Telephone').value ;
-                let groupe=document.getElementById('Groupe').value ;
-                let bio=document.getElementById('Bio').value ;
-                let photo=document.getElementById('Avatar').value;
-                let email=document.getElementById('Email').value ;
-                let modification ={'id' :id,'prenom': prenom,'nom' : nom,'telephone':telephone,'groupe' : groupe,'email':email,'bio':bio,'photo': photo};
-                if(contact['id'] == iModifier.textContent)
-                {
-                    let index = contacts.indexOf(contact);
-                    contacts.push(modification);
-                    contacts.splice(index,1);
-                }
-                   
-                   
-                    
-                   
-
-                });
-
-               }
-            });
-
-            } ); 
-            
-            
-
-        }
-}
-
-
-window.addEventListener("load",afficherContacte());
 
